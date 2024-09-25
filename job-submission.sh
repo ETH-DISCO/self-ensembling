6b9d3b3e679944312b3733c6b2538b4e245df85f
sueszli <jabaryyahya@gmail.com>
2024-09-25 02:10:41 +0200

    up

--- job-submission.sh
+++ job-submission.sh
@@ -22,40 +22,37 @@ alias smon_mine="grep --color=always --extended-regexp '${USER}|$' /home/sladmit
 alias watch_smon_free="watch --interval 300 --no-title --differences --color \"grep --color=always --extended-regexp 'free|$' /home/sladmitet/smon.txt\""
 alias watch_smon_mine="watch --interval 300 --no-title --differences --color \"grep --color=always --extended-regexp '${USER}|$' /home/sladmitet/smon.txt\""
 
-# ------------------------------------
+# ---------------------------------------------
 
-# change dirs
 cd /itet-stor/$USER/net_scratch/
 rm -rf /itet-stor/$USER/net_scratch/slurm # clean up previous slurm output
 
 # clone this repository
 rm -rf self-ensembling
-git clone https://github.com/ETH-DISCO/self-ensembling
+git clone https://github.com/ETH-DISCO/self-ensembling/
 cd self-ensembling
 sed -i 's/{{USERNAME}}/'$USER'/g' job.sh # insert username into template
 
 # install conda
 chmod +x ./install-conda.sh && ./install-conda.sh
-eval "$(/itet-stor/${USER}/net_scratch/conda/bin/conda shell.bash hook)"
-echo '[[ -f /itet-stor/${USER}/net_scratch/conda/bin/conda ]] && eval "$(/itet-stor/${USER}/net_scratch/conda/bin/conda shell.bash hook)"' >> /home/$USER/.bashrc
 
-# create conda env
+# remove previous env (if exists)
 [[ -f /itet-stor/${USER}/net_scratch/conda/bin/conda ]] && eval "$(/itet-stor/${USER}/net_scratch/conda/bin/conda shell.bash hook)" # conda activate base
-rm -rf /itet-stor/yjabary/net_scratch/conda_envs/con && conda remove --yes --name con --all || true # remove if exists
+rm -rf /itet-stor/$USER/net_scratch/conda_envs/con && conda remove --yes --name con --all || true # remove previous env if exists
+
+# create new env
 conda env create --file environment.yml
 conda activate con
 python3 -c "import torch; print(torch.__version__)"
 conda deactivate
 
 # dispatch job
-sbatch job.sh ./src/0_hyperparam_optim_v2.py
+sbatch job.sh
 
 # check results
 watch -n 1 "squeue | grep $USER"
-
-# check logs
 for file in /itet-stor/$USER/net_scratch/slurm/*; do if [ -f "$file" ]; then echo -e "\e[32m$(basename "$file")\e[0m"; cat "$file"; echo -e "\n----------\n"; fi; done
 
-# clean up
-[[ -f /itet-stor/${USER}/net_scratch/conda/bin/conda ]] && eval "$(/itet-stor/${USER}/net_scratch/conda/bin/conda shell.bash hook)" # conda activate base
-conda remove --yes --name con --all
+
+# dispatch job
+sbatch job.sh ./src/0_hyperparam_optim_v2.py
