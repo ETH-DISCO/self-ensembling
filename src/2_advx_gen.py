daa7890b76ae064ee2b8a727a688e62fc49f4945
sueszli <jabaryyahya@gmail.com>
2024-10-14 11:50:20 +0200

    up

--- src/2_advx_gen.py
+++ src/2_advx_gen.py
@@ -76,11 +76,11 @@ def eval(config: dict):
     # with torch.no_grad():
     with torch.inference_mode(), torch.amp.autocast(device_type=("cuda" if torch.cuda.is_available() else "cpu"), enabled=(torch.cuda.is_available())):
         if not run_cheap:
-            # run attack on all test points at once
+            # complete version
             adv_complete = adversary.run_standard_evaluation(x_test, y_test, bs=batch_size, state_path=weights_path / "restnet152_advx_state.pth")
             torch.save({"adv_complete": adv_complete}, output_path / "restnet152_advx.pth")
         else:
-            # individual version, each attack is run on all test points
+            # individual version
             adv_complete = adversary.run_standard_evaluation_individual(x_test, y_test, bs=batch_size)
             torch.save(adv_complete, output_path / "restnet152_advx_individual.pth")
 
