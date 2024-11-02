063ad13d3f44ee9397f9caeac9740ff13c722317
sueszli <jabaryyahya@gmail.com>
2024-11-02 16:11:04 +0100

    up

--- src/utils.py
+++ src/utils.py
@@ -65,7 +65,7 @@ def timeit(func) -> callable:
         start = time.time()
         result = func(*args, **kwargs)
         end = time.time()
-        print(f"{func.__name__} took {end - start:.2f} seconds")
+        print(f"{func.__name__} executed in {end - start:.2f}s")
         return result
 
     return wrapper
