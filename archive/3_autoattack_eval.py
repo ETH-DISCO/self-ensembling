0a8b44302ff2e35e1679feaa9eabb7daaa1eef85
sueszli <jabaryyahya@gmail.com>
2024-11-03 09:59:06 +0100

    up

--- archive/3_autoattack_eval.py
+++
@@ -1,4 +0,0 @@
-from dataloader import get_cifar10_apgdce_apgdt, get_cifar100_apgdce_apgdt
-
-cifar10_apgdce, cifar10_apgdt = get_cifar10_apgdce_apgdt()
-cifar100_apgdce, cifar100_apgdt = get_cifar100_apgdce_apgdt()
