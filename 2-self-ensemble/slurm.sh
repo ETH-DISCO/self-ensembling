05cd416e5d5d2dbff62f542043dd56a2a06eaee6
sueszli <jabaryyahya@gmail.com>
2024-11-28 02:47:43 +0100

    up

--- 2-self-ensemble/slurm.sh
+++ 2-self-ensemble/slurm.sh
@@ -1,4 +1,5 @@
-# srun --mem=100GB --gres=gpu:01 --nodelist tikgpu10 --pty bash -i
+# $ srun --mem=100GB --gres=gpu:01 --nodelist tikgpu10 --pty bash -i
+# $ salloc --nodelist=tikgpu10
 
 # 
 # 1. generate a conda environment
