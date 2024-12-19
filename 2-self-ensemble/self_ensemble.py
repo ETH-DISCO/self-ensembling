ac73cace135630eea4c1f653d1b845a7948d0c89
sueszli <jabaryyahya@gmail.com>
2024-12-19 18:57:56 +0100

    up

--- 2-self-ensemble/self_ensemble.py
+++ 2-self-ensemble/self_ensemble.py
@@ -88,7 +88,7 @@ def make_multichannel_input(images, enable_noise, enable_random_shuffle, resolut
     down_noise = 0.2  # noise standard deviation to be added at the low resolution
     up_noise = 0.2  # noise stadard deviation to be added at the high resolution
     jit_size = 3  # max size of the x-y jit in each axis, sampled uniformly from -jit_size to +jit_size inclusive
-    up_res = images.shape[-1]  # use actual image size instead of hardcoded 32 for CIFAR-10 or CIFAR-100
+    up_res = images.shape[-1]
 
     if not enable_noise:
         return torch.concatenate([images] * len(resolutions), axis=1)  # don't do anything
