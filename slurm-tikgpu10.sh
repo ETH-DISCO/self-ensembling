d52e22f7e09ab9bcecddfcad1feb30f7a1c471b9
sueszli <jabaryyahya@gmail.com>
2024-12-29 20:31:05 +0100

    up

--- slurm-tikgpu10.sh
+++ slurm-tikgpu10.sh
@@ -1,3 +1,5 @@
+# salloc --mem=10GB --nodelist=tikgpu10
+
 # clone project
 mkdir -p /scratch/$USER
 cd /scratch/$USER
@@ -15,32 +17,11 @@ if conda env list | grep -q "^con "; then
 fi
 conda env create --file environment.yml
 
-#
 # dispatch
-#
+sbatch --output=$(pwd)/%j.out --error=$(pwd)/%j.err --nodelist=$(hostname) --mem=150G --nodes=1 --gres=gpu:1 --wrap="bash -c 'source /itet-stor/${USER}/net_scratch/conda/etc/profile.d/conda.sh && conda activate con && python3 $(pwd)/1-resnet/resnet.py'"
+sbatch --output=$(pwd)/%j.out --error=$(pwd)/%j.err --nodelist=$(hostname) --mem=150G --nodes=1 --gres=gpu:1 --wrap="bash -c 'source /itet-stor/${USER}/net_scratch/conda/etc/profile.d/conda.sh && conda activate con && python3 $(pwd)/2-self-ensemble/self_ensemble.py'"
 
-sbatch \
-    --output=$(pwd)/%j.out \
-    --error=$(pwd)/%j.err \
-    --nodelist=$(hostname) \
-    --mem=150G \
-    --nodes=1 \
-    --gres=gpu:1 \
-    --wrap="bash -c 'source /itet-stor/${USER}/net_scratch/conda/etc/profile.d/conda.sh && conda activate con && python3 $(pwd)/1-resnet/resnet.py'"
-
-sbatch \
-    --output=$(pwd)/%j.out \
-    --error=$(pwd)/%j.err \
-    --nodelist=$(hostname) \
-    --mem=150G \
-    --nodes=1 \
-    --gres=gpu:1 \
-    --wrap="bash -c 'source /itet-stor/${USER}/net_scratch/conda/etc/profile.d/conda.sh && conda activate con && python3 $(pwd)/2-self-ensemble/self_ensemble.py'"
-
-#
-# monitoring
-#
-
+# monitor
 watch -n 0.5 "squeue -u $USER --states=R"
 tail -f $(ls -v $(pwd)/*.err 2>/dev/null | tail -n 300)
 tail -f $(ls -v $(pwd)/*.out 2>/dev/null | tail -n 300)
