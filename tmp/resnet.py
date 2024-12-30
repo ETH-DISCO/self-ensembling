2a01fa4c0efc5ccce89bea7fd69e504557f04e78
sueszli <jabaryyahya@gmail.com>
2024-12-30 02:52:36 +0100

    up

--- tmp/resnet.py
+++ tmp/resnet.py
@@ -1,3 +1,7 @@
+"""
+pip install contourpy==1.3.1 cycler==0.12.1 filelock==3.16.1 fonttools==4.55.0 fsspec==2024.10.0 jinja2==3.1.4 kiwisolver==1.4.7 markupsafe==3.0.2 matplotlib==3.9.2 mizani==0.13.0 mpmath==1.3.0 networkx==3.4.2 numpy==2.1.3 packaging==24.2 pandas==2.2.3 patsy==1.0.1 pillow==11.0.0 plotnine==0.14.1 pyparsing==3.2.0 python-dateutil==2.9.0.post0 pytz==2024.2 scipy==1.14.1 six==1.16.0 statsmodels==0.14.4 sympy==1.13.1 torch==2.5.1 torchvision==0.20.1 tqdm==4.67.0 typing-extensions==4.12.2 tzdata==2024.2
+"""
+
 import hashlib
 import json
 import os
